59b599ad7762e8db67788d96ffa6d228afa2a25e
chanpus <t1000v@naver.com>
2023-09-28 10:26:44 +0900

    .

--- src/main/java/hello/hellospring/service/springConfig.java
+++ src/main/java/hello/hellospring/service/springConfig.java
@@ -1,22 +1,37 @@
 package hello.hellospring.service;
 
 
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 
 @Configuration
 public class springConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public springConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+    //@Bean
+    //public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+    //}
 
 }
